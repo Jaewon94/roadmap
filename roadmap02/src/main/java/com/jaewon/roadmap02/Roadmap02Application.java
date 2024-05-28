3e6a2f15cac040db03b64f6ee9345ea1823dfa01
Jaewon94 <wodnjs3580@gmail.com>
2024-05-28 19:36:51 +0900

    roadmap02 init

---
+++ roadmap02/src/main/java/com/jaewon/roadmap02/Roadmap02Application.java
@@ -0,0 +1,13 @@
+package com.jaewon.roadmap02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Roadmap02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Roadmap02Application.class, args);
+    }
+
+}
---
+++ roadmap02/src/test/java/com/jaewon/roadmap02/Roadmap02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaewon.roadmap02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Roadmap02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
