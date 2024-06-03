fd5b850adea753fc6c01cfc2d3d2a1c85f8dcbe7
Jaewon94 <wodnjs3580@gmail.com>
2024-06-03 16:30:37 +0900

    init

---
+++ roadmap03/src/test/java/com/jaewon/roadmap03/Roadmap03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaewon.roadmap03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Roadmap03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
