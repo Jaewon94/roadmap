9341a86383ea9cc0a069831430aa3fc9fdae8dae
Jaewon94 <wodnjs3580@gmail.com>
2024-06-04 15:06:41 +0900

    스프링 MVC 1편 - 백엔드 웹 개발 핵심 기술 - section2

--- roadmap03/src/main/java/com/jaewon/roadmap03/Roadmap03Application.java
+++ roadmap03/src/main/java/com/jaewon/roadmap03/Roadmap03Application.java
@@ -2,7 +2,9 @@ package com.jaewon.roadmap03;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan   // 서블릿 자동 등록
 @SpringBootApplication
 public class Roadmap03Application {
 
